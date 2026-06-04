644376c7661e69d761c2a6f1fee6a5ef98d6187e
tahahajjout <taha2000hajjout@gmail.com>
2026-06-04 10:21:20 +0200

    connection backend to cloud database "aiven"

--- Artisant-backend/config/db.js
+++ Artisant-backend/config/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT,
     password: process.env.DB_PASSWORD,
-    database: 'artisans_db',
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10
 });
