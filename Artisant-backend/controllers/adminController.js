7da50c5b761b1d320bdbf642ca3a70d26fdc5c40
tahahajjout <taha2000hajjout@gmail.com>
2026-06-06 00:56:44 +0200

    transport declared twice fix

--- Artisant-backend/controllers/adminController.js
+++ Artisant-backend/controllers/adminController.js
@@ -84,13 +84,13 @@ exports.deleteArtisan = async (req, res) => {
 
 const sendStatusEmail = async (artisan, status) => {
     const nodemailer = require('nodemailer');
-     const transporter = nodemailer.createTransport({
+    /* const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
             user: process.env.MAIL_USER,
             pass: process.env.MAIL_PASS
         }
-    }); 
+    });*/
         const transporter = nodemailer.createTransport({
         host: 'smtp-relay.brevo.com',
         port: 465,
